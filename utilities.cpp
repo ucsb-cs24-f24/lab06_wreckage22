04751b03d9b1626277e1fa0327ba6b56f90950e3
wreckage22 <abdullah_mohammed@ucsb.edu>
2024-11-20 16:58:39 -0800

    COMMIT MESSAGE

--- utilities.cpp
+++ utilities.cpp
@@ -36,40 +36,44 @@ void MovieSet2::insertMovie2(Movie2 m){
 // }
 
 void MovieSet2::printMovie2(std::vector<std::string> prefixes) const {
-    std::vector<std::pair<std::string, Movie2>> bestMovies; 
+    std::vector<std::pair<std::string, Movie2>> bestMovies;
 
-    for (const auto& pref : prefixes) { 
+    for (size_t i = 0; i < prefixes.size(); ++i) {
+        const auto& prefix = prefixes[i];
         std::vector<Movie2> matchingMovies;
-        int prefSize = pref.size();
+        int prefSize = prefix.size();
 
-        for (const auto& movie2 : movies2) {
-            std::string moviePref = movie2.getSubstr(0, prefSize);
-            if (moviePref == pref) {
-                matchingMovies.push_back(movie2);
+        for (const auto& movie : movies2) {
+            if (movie.getMovieName2().substr(0, prefSize) == prefix) {
+                matchingMovies.push_back(movie);
             }
         }
 
         if (matchingMovies.empty()) {
-            bestMovies.emplace_back(pref, Movie2("No movie found", 0.0));
-        } else {
+            bestMovies.emplace_back(prefix, Movie2("No movie found", 0.0));
+        } 
+        else {
             std::sort(matchingMovies.begin(), matchingMovies.end());
-
             for (const auto& movie : matchingMovies) {
                 std::cout << movie.getMovieName2() << ", " << movie.getMovieRating2() << "\n";
             }
-
-            bestMovies.emplace_back(pref, matchingMovies[0]);
         }
 
-        std::cout << "\n";
+        if (i != prefixes.size() - 1) {
+            std::cout << "\n";
+        }
+
+        if (!matchingMovies.empty()) {
+            bestMovies.emplace_back(prefix, matchingMovies.front());
+        }
     }
 
     for (const auto& [prefix, bestMovie] : bestMovies) {
         if (bestMovie.getMovieName2() == "No movie found") {
             std::cout << "No movies found with prefix " << prefix << "\n";
         } else {
-            std::cout << "Best movie with prefix " << prefix << " is: " 
-                      << bestMovie.getMovieName2() << " with rating " 
+            std::cout << "Best movie with prefix " << prefix << " is: "
+                      << bestMovie.getMovieName2() << " with rating "
                       << bestMovie.getMovieRating2() << "\n";
         }
     }
